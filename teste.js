a5ee461d14ea9d7a93e1a49c782344aa66f21ba7
henrique <henrique@kodeninja.tech>
2022-01-11 20:37:26 -0300

    exemplos da aula 1

---
+++ teste.js
@@ -0,0 +1,22 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNUmero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNUmero;
+
+console.log(operacaoMatematica)
+
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+
