538f3394f8c2bc753e6cc73e7f75b02b075245cb
henrique <henrique@kodeninja.tech>
2022-01-12 21:22:50 -0300

    operadores e funcoes

---
+++ funcao.js
@@ -0,0 +1,16 @@
+// function cumprimentar(){
+//     return 'Oi gente!'
+//    }
+   
+//    function cumprimentaPessoa(nomePessoa) {
+//     console.log(`${cumprimentar()} Meu nome é ${nomePessoa}`)
+//    }
+   
+//    cumprimentaPessoa('Paula') // “Oi gente! Meu nome é Paula”
+
+
+   function operacaoMatematica(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3
+   }
+   
+   console.log (operacaoMatematica(15, 30, 45)) // 90
